0a294894c86071d8dffc4e53c986743020c54e10
cassio-mendes <kassiogithub@gmail.com>
2026-03-19 13:54:32 -0300

    Testando caminho /hello

--- src/main/java/com/example/AplicaoTeste/AplicaoTesteApplication.java
+++ src/main/java/com/example/AplicaoTeste/AplicaoTesteApplication.java
@@ -2,14 +2,22 @@ package com.example.AplicaoTeste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@RestController
 public class AplicaoTesteApplication {
 
 	static void main(String[] args) {
         SpringApplication.run(AplicaoTesteApplication.class, args);
-
-
 	}
+
+    @GetMapping("/hello")
+    public static String hello(@RequestParam(value = "name", defaultValue = "Mundo") String name) {
+        return String.format("Olá %s!", name);
+    }
 
 }
