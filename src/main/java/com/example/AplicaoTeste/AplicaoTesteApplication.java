fad6fcfded1d57c479773873859dbcbaf9855f66
cassio-mendes <kassiogithub@gmail.com>
2026-03-19 12:55:20 -0300

    First Commit

---
+++ src/main/java/com/example/AplicaoTeste/AplicaoTesteApplication.java
@@ -0,0 +1,15 @@
+package com.example.AplicaoTeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicaoTesteApplication {
+
+	static void main(String[] args) {
+        SpringApplication.run(AplicaoTesteApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/example/AplicaoTeste/AplicaoTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AplicaoTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicaoTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
