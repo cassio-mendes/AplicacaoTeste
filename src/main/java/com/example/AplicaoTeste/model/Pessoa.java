bae1ba57c196603d16eeed4863f147a04fdc5165
cassio-mendes <kassiogithub@gmail.com>
2026-03-19 13:54:56 -0300

    Modelando classe Pessoa

--- src/main/java/com/example/AplicaoTeste/model/Pessoa.java
+++ src/main/java/com/example/AplicaoTeste/model/Pessoa.java
@@ -1,4 +1,17 @@
 package com.example.AplicaoTeste.model;
 
 public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private String cpf;
+    private String profissao;
+
+    public Pessoa(String nome, int idade, String cpf, String profissao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.profissao = profissao;
+    }
+
 }
